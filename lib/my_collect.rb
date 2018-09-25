c326cda32cd7da7f8ac086b0008d49a6d688409f
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-25 02:35:19 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   new_collection = Array.new
   while i < array.length
-    new_collection << yield(array[i])
+    new_collection << yield(collection[i])
     i += 1
   end
     collection
