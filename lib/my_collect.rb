9a7c7827d7192f166ce3a531beed8eefdc0ecfc3
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-25 02:30:48 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   collection = Array.new
   if i < array.length
     i += 1
-    collection << yield(array[i].upcase)
+    collection << yield(array[i])
   end
     collection
 end
